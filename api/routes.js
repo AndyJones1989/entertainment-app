53dcdff374e8a4642fa67f540cf39139aec5dfc0
JDawg <andrew.jones7@ntlworld.com>
2023-05-13 20:10:13 +0100

    jwt works, but boy is it ugly

---
+++ api/routes.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require ('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('some fool be talkin');
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, Access-Control-Allow-Headers, Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, append,delete,entries,foreach,get,has,keys,set,values,Authorization");
+    res.header('Access-Control-Allow-Methods', "POST, GET, OPTIONS, DELETE, PUT");
+    next();
+})
+
+app.options('/', (req, res, next)=>{
+    console.log('useRoute');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Access-Control-Allow-Headers, Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, append,delete,entries,foreach,get,has,keys,set,values,Authorization");
+    res.status(200).send();
+})
+app.use(bodyParser.json());
+
+app.post('/login', (req, res, next) => {
+    console.log(req.body);
+    const { body } = req;
+    const { email } = body.user;
+    const { password } = body.user;
+
+    const username='andy@andy.com';
+    const passwordTest='1';
+
+    if(email === username && password === passwordTest) { 
+        jwt.sign({email}, process.env.JWT_KEY, { expiresIn: '3h' },(err, token) => {
+            if(err) { console.log(err) }    
+            res.send(token);
+        });
+    } else {
+        res.status(403).send('Invalid User');
+    }
+})
+
+app.listen(process.env.PORT || 3001, ()=> {console.log("App is available on http://localhost:3001")});
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,43 @@
+'use client'
+import axios from "axios";
+import { useState } from "react";
 
 
-export default function login() {
+export default function Login() {
+  console.log(process.env.NEXT_PUBLIC_API_URL + 'login/');
+
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+
+const handleSubmit = async (event:any) => {
+  event.preventDefault();
+  const user = {
+    email: emailInput,
+    password: passwordInput
+  }
+
+  var headers = {
+    'Content-Type': 'application/json', 
+    "Access-Control-Allow-Origin": "*"
+}
+
+  const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + 'login', { user } );
+  console.log(response)
+};
+
+const handleEmailChange = (event)=>{
+  setEmailInput(event.target.value);
+};
+
+const handlePasswordChange = (event)=>{
+  setPasswordInput(event.target.value);
+};
 
     return (
-    <>
-    <div>login page</div>
-    </>
+    <form onSubmit={handleSubmit}>
+    <input type='email' onChange={handleEmailChange}></input>
+    <input type='password' onChange={handlePasswordChange}></input>
+    <button >submit</button>
+    </form>
     );
   }
