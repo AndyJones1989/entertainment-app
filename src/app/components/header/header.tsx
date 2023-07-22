bdec0a35cb898ddabcfdbdc270717e69015ec02e
AndyJones1989 <126464914+AndyJones1989@users.noreply.github.com>
2023-07-22 21:59:45 +0100

    Holiday work (#4)
    * improved post event form, added auth route
    
    * ninja api req added, now needs filtering and integrating with Mongo
    
    * post event working with mongo
    
    * Working with EC2 backend
    
    * window errors fixed
    
    * router build issue wrapped in isClient
    
    * add home icon and profile link
    
    * add burger menu and improve link hover states
    
    * include new svg

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -5,17 +5,14 @@ import ProfileSVG from '../../../../assets/profile';
 import ProfileAuthSVG from '../../../../assets/profile-logged-in';
 import { useMediaQuery } from '../../../../utils/use-media-query';
 import { sizes } from '../../../../utils/use-media-query';
-import React, { useContext, useEffect } from 'react';
-import { AuthContext } from '@/app/context/auth-provider';
+import  { useState } from 'react';
 import Link from 'next/link';
 import { HydrationProvider, Client } from 'react-hydration-provider';
-
-export interface IHeaderProps {
-    isLoggedIn: boolean
-}
+import HomeSvg from '../../../../assets/home';
 
 
-const Header = ({isLoggedIn}: IHeaderProps): JSX.Element => {
+const Header = (): JSX.Element => {
+   const [showMenu, setShowMenu] = useState(false);
    const isMobile =  useMediaQuery(sizes.sm);
    let email;
    if(typeof window !== 'undefined'){
@@ -24,24 +21,39 @@ const Header = ({isLoggedIn}: IHeaderProps): JSX.Element => {
 
    const displayName = email?.split("@")[0];
 
+   const handleMenuClick = () => {
+         setShowMenu(!showMenu);
+    };
+
+
     return(
         <HydrationProvider>
             <Client>
         <nav className={classes.wrapper}>
             {isMobile && 
-            <button className={classes.iconWrapper} >
+            <>
+            <button className={classes.iconWrapper} onClick={handleMenuClick}>
             <MenuSVG/>
             </button>
+            <ul className={showMenu ? classes.showMenu : classes.hideMenu}>
+            <Link href='/' className={classes.burgerElement} >Home</Link>
+            <Link href='/landing' className={classes.burgerElement}>Show Activities Near Me</Link>
+            <Link href='/post-event' className={classes.burgerElement}>Post an Activity</Link>
+            <Link href='/login' className={classes.burgerElement} >Account</Link>
+            <button className={classes.burgerClose} onClick={handleMenuClick}>Close</button>
+            </ul>
+            </>
             }
             
             {!isMobile &&
             <>
+            <HomeSvg/>
             <Link href='/landing' className={classes.headerElement}>Show Activities Near Me</Link>
             <Link href='/post-event' className={classes.headerElement}>Post an Activity</Link>
             </>
             }
             <button className={classes.iconWrapper}>
-            <p className={classes.headerElement}>{displayName ? displayName : 'Profile'}</p>
+            <Link className={classes.headerElement} href='/login'>{displayName ? displayName : 'Profile'}</Link>
             {email ? <ProfileAuthSVG/> : <ProfileSVG/>}
             </button>
         </nav>
